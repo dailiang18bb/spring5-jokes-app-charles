cd45bab15118b05f3491797c71c41bf3756a4a14
Charles <dailiang18bb@gmail.com>
2020-07-11 19:35:54 -0400

    XML base config example

--- src/main/java/charles/spring/joke/spring5jokesappcharles/config/ChuckConfiguration.java
+++ src/main/java/charles/spring/joke/spring5jokesappcharles/config/ChuckConfiguration.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-@Configuration
+// @Configuration
 public class ChuckConfiguration {
 
-    @Bean
+    // @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
